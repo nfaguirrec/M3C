a71bba1752093b2ad927fe111b3f3c9acd709001
nfaguirrec <nfaguirrec@gmail.com>
2016-02-21 23:51:18 +0100

    1) El muestreo de la energia rotacional ya esta listo 2) He implementado la version de GAUSSIAN para mejorar la energia 3) Las reacciones se puede ver con detalles

--- src/gamessDriver.sh
+++ src/gamessDriver.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+##################################################################
+#
+#  This file is part of M3C
+#  Copyright (C) by authors (2012-2016)
+#  
+#  Authors:
+#    * Dr. Néstor F. Aguirre (2012-2016)
+#          nestor.aguirre@uam.es
+#    * Dr. Sergio Díaz-Tendero (2012-2015)
+#          sergio.diaztendero@uam.es
+#    * Prof. M. Paul-Antoine Hervieux (2012-2015)
+#          Paul-Antoine.Hervieux@ipcms.unistra.fr
+#    * Prof. Fernando Martín (2012-2015)
+#          fernando.martin@uam.es
+#    * Prof. Manuel Alcamí (2012-2015)
+#          manuel.alcami@uam.es
+#  
+#  Redistribution and use in source and binary forms, with or
+#  without modification, are permitted provided that the
+#  following conditions are met:
+#  
+#   * Redistributions of binary or source code must retain
+#     the above copyright notice and this list of conditions
+#     and/or other materials provided with the distribution.
+#   * All advertising materials mentioning features or use of
+#     this software must display the following acknowledgement:
+#     
+#     This product includes software from M3C project.
+#
+##################################################################
 
 declare -A ATOMIC_NUMBER
 
